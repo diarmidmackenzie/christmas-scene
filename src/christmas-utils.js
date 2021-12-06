1d11d6675f5fc7b6d298ce8054a3cdd96b7436f8
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2021-12-06 17:08:39 +0000

    Assets + test page with christmas tree.

---
+++ src/christmas-utils.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent('cylindrical-position', {
+  schema: {    
+    height: {type: 'number', default: 1.6},
+    radius: {type: 'number', default: 1},
+    angle: {type: 'number', default: 0},
+    faceInward: {type: 'boolean', default: true},
+  },
+
+  update() {
+    const radians = Math.PI * this.data.angle / 180
+
+    this.el.object3D.position.x = this.data.radius * Math.sin(radians)
+    this.el.object3D.position.y = this.data.height
+    this.el.object3D.position.z = -this.data.radius * Math.cos(radians)
+
+    this.el.object3D.rotation.x = 0
+    this.el.object3D.rotation.y = -radians + (this.data.faceInward ? 0 : Math.PI)
+    this.el.object3D.rotation.z = 0
+  },
+});
