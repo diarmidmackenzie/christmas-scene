5d014a285ad7f5d4d26c37c8007e31d319e33049
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2021-12-13 19:45:47 +0000

    Ribbon for wrapping presents.

--- research/composite-objects/custom-geometries.js
+++ research/composite-objects/custom-geometries.js
@@ -178,21 +178,49 @@ AFRAME.registerGeometry('ribbon', {
   init: function (data) {
 
     // used to avoid z-fighting.
-    const delta = 0.00001
+    const delta = 0.001
     const geometries = [];
 
-    geometries.push(new THREE.PlaneGeometry(data.ribbon, data.height));
     // front ribbon
-    geometries[1].translate(0, data.depth/2, 0);
     geometries.push(new THREE.PlaneGeometry(data.ribbon, data.height));
+    geometries[0].translate(0, 0, data.depth/2 + delta);
     // back ribbon
-    geometries[2].rotateY(0, Math.PI, 0);
-    geometries[2].translate(0, -data.depth/2, 0);
+    geometries.push(new THREE.PlaneGeometry(data.ribbon, data.height));
+    geometries[1].rotateY(Math.PI);
+    geometries[1].translate(0, 0, -data.depth/2 - delta);
+    // left ribbon
+    geometries.push(new THREE.PlaneGeometry(data.ribbon, data.height));
+    geometries[2].rotateY(-Math.PI/2);
+    geometries[2].translate(-data.width/2 - delta, 0, 0);
+    // right ribbon
+    geometries.push(new THREE.PlaneGeometry(data.ribbon, data.height));
+    geometries[3].rotateY(Math.PI/2);
+    geometries[3].translate(data.width/2 + delta, 0, 0);
+    // top ribbons
+    geometries.push(new THREE.PlaneGeometry(data.ribbon, data.depth));
+    geometries[4].rotateX(-Math.PI/2);
+    geometries[4].translate(0, data.height/2 + delta, 0);
+    geometries.push(new THREE.PlaneGeometry(data.width, data.ribbon));
+    geometries[5].rotateX(-Math.PI/2);
+    geometries[5].translate(0, data.height/2+ delta, 0);
 
-    // To be completed...
+    // bottom ribbons
+    geometries.push(new THREE.PlaneGeometry(data.ribbon, data.depth));
+    geometries[6].rotateX(Math.PI/2);
+    geometries[6].translate(0, -data.height/2 - delta, 0);
+    geometries.push(new THREE.PlaneGeometry(data.width, data.ribbon));
+    geometries[7].rotateX(Math.PI/2);
+    geometries[7].translate(0, -data.height/2 - delta, 0);
+
+    // rosette
+    geometries.push(new THREE.TorusKnotGeometry(data.ribbon, data.ribbon/4, 30, 3, 7, 11));
+    geometries[8].rotateX(Math.PI/2);
+    geometries[8].translate(0, data.height/2 + data.ribbon/3 + delta, 0);
 
     this.geometry = THREE.BufferGeometryUtils.mergeBufferGeometries(geometries);
 
-    recenterGeometry(this.geometry);
+    // *don't* recenter geometry, as the rosette will ruin the offset vs. a box we
+    // wrap the ribbon around.
+    //recenterGeometry(this.geometry);
   }
 });
