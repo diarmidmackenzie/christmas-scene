9dd03bc2255326ae279df757871172433e2db76d
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-12-07 13:01:21 +0000

    Watch all JS source

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   // Output directory: _site
 
   eleventyConfig.addPassthroughCopy("assets/**");
+  eleventyConfig.addWatchTarget("./src/**");
   return {
     dir: {
       includes: "./src/html" 
