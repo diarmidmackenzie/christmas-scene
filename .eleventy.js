6981c7a5afefbea0014990c59a11630e5b24224d
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-12-07 11:03:07 +0000

    Start moving content into modules

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,9 @@ module.exports = function(eleventyConfig) {
   // Output directory: _site
 
   eleventyConfig.addPassthroughCopy("assets/**");
+  return {
+    dir: {
+      includes: "./src/html",
+    }
+  }
 };
