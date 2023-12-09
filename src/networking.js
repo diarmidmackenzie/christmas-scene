6f0d4c47da5a41b2b36a135117b84f32afd191bf
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-12-09 15:28:58 +0000

    Working audio bridge

--- src/networking.js
+++ src/networking.js
@@ -5,7 +5,7 @@ AFRAME.registerComponent('networking', {
     this.el.setAttribute('networked-scene', 
                          `serverURL: christmas-scene-naf-server.glitch.me;
                           room: ${ROOM_KEY};
-                          adapter: wseasyrtc;
+                          adapter: easyrtc;
                           audio: true;`)
   }
 })
