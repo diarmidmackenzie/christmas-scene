1a702884c86ea068b2ce31c7f841b6b1bdeb01b9
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-12-15 18:30:16 +0000

    Add 2D front screen with room change options.

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,9 @@ module.exports = function(eleventyConfig) {
   // Output directory: _site
 
   eleventyConfig.addPassthroughCopy("assets/**");
+  eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addWatchTarget("./src/**");
+  eleventyConfig.addWatchTarget("styles.css");
   return {
     dir: {
       includes: "./src/html" 
