69a839ce60989034b3db16b4466253ab5a00d3ab
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-12-15 18:32:29 +0000

    Make sure generated codes are always 4 characters

--- src/room-key.js
+++ src/room-key.js
@@ -1,4 +1,4 @@
-const randomString = () => (Math.random() + 1).toString(36).substring(8).toUpperCase();
+const randomString = () => (Math.random() + 1).toString(36).substring(6, 10).toUpperCase();
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const ROOM_KEY = urlParams.get('room') 
